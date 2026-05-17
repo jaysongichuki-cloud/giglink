b872ab0c8d3472b9d2bcec995c8ce780fe0cb15f
jaysongichuki-cloud <jayson.gichuki@student.moringaschool.com>
2026-05-17 22:50:38 +0300

    Add files via upload

--- client.js
+++ client.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 
-
-const baseURL = `${import.meta.env.VITE_API_URL}/project/1` || '/api'
+const baseURL = import.meta.env.VITE_API_URL || '/api'
 
 const client = axios.create({
   baseURL,
-  headers: { 'Content-Type': 'application/json' },
+  headers: { 'Content-Type': 'application/json', },
 })
 
 export default client
