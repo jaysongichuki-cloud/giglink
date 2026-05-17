c9cdef18765a6cd88fc21affc4e2a51a712407d4
jaysongichuki-cloud <jayson.gichuki@student.moringaschool.com>
2026-05-17 22:55:33 +0300

    Add files via upload

--- client.js
+++ client.js
@@ -4,7 +4,23 @@ const baseURL = import.meta.env.VITE_API_URL || '/api'
 
 const client = axios.create({
   baseURL,
-  headers: { 'Content-Type': 'application/json', },
+  headers: { 'Content-Type': 'application/json' },
 })
+
+
+client.interceptors.response.use(
+  (response) => {
+    
+    if (typeof response.data === 'string') {
+      try {
+        return JSON.parse(response.data);
+      } catch (e) {
+        return response.data;
+      }
+    }
+    return response.data;
+  },
+  (error) => Promise.reject(error)
+)
 
 export default client
